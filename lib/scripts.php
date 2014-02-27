62c1208bf6692218d2245b2dda7757fd1a42ae31
Kelly Anne <kpipe@sju.edu>
2014-02-27 14:33:56 -0500

    Navbar Changes

--- lib/scripts.php
+++ lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'd341d633d43da83890724955d566b0f4');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'daf10c46d7cec7a22bbc3bf8bd23c669');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
