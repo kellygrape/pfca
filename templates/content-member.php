0746e529ead6d66795e5e00e07a4946c21c5aa79
Kelly Anne <kpipe@sju.edu>
2014-02-27 17:34:53 -0500

    Fix for previous commit

--- templates/content-member.php
+++ templates/content-member.php
@@ -10,11 +10,9 @@
       <p><?php the_field('podcast_tagline'); ?></p>
       <h3>Member Information</h3>
       <ul>
-        <?php if(get_field('member_journalism_alias')): ?><li><strong>Journalism Alias</strong> <?php the_field('member_journalism_alias'); ?></li><?php endif; ?>
-        <li><strong>Podcast</strong> ________</li>
-        
+        <?php if(get_field('member_journalism_alias')): ?><li><strong>Journalism Alias</strong> <?php the_field('member_journalism_alias'); ?></li><?php endif; ?>        
         <?php $podcast = get_field('member_podcast'); ?>
-        <li><strong>Podcast</strong> <a href="<?php the_permalink($podcast->ID) ?>"><?php get_the_title($podcast->ID);?></a></li>
+        <li><strong>Podcast</strong> <a href="<?php echo get_permalink($podcast->ID) ?>"><?php echo get_the_title($podcast->ID);?></a></li>
         <?php if(get_field('member_title')): ?><li><strong>Title</strong> <?php the_field('member_title'); ?></li><?php endif; ?>
       </ul>
       <?php the_content(); ?>
