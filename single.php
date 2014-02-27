07ca8f3a94513f15c85a1cc23910601b38075494
Kelly Anne <kpipe@sju.edu>
2014-02-27 16:26:35 -0500

    Post type debug

--- single.php
+++ single.php
@@ -1,12 +1,13 @@
-<h1><?php echo get_post_type();?></h1>
-
 <?php if ( 'pfca_podcast' == get_post_type() ): ?>
+<h1>Podcast!</h1>
 <?php get_template_part('templates/content', 'podcast'); ?>
 
 <?php elseif ( 'pfca_member' == get_post_type() ): ?>
+<h1>Member!</h1>
 <?php get_template_part('templates/content', 'member'); ?>
 
 <?php else: ?>
+<h1>Single</h1>
 <?php get_template_part('templates/content', 'single'); ?>
 
 <?php endif; ?>
