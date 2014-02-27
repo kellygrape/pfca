e09709b1a0498e65aa5d6168a55765fa0f57718e
Kelly Anne <kpipe@sju.edu>
2014-02-27 15:27:38 -0500

    Content types

--- single.php
+++ single.php
@@ -1,1 +1,10 @@
+<?php if ( 'podcast' == get_post_type() ): ?>
+<?php get_template_part('templates/content', 'podcast'); ?>
+
+<?php elseif ( 'member' == get_post_type() ):: ?>
+<?php get_template_part('templates/content', 'podcast'); ?>
+
+<?php else: ?>
 <?php get_template_part('templates/content', 'single'); ?>
+
+<?php endif; ?>
