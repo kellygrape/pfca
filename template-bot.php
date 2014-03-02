5c2b0e042f8450af5cc129c2ac08ed67166660bd
Kelly Anne <geeksch1@box777.bluehost.com>
2014-03-02 14:57:13 -0700

    BOT page

---
+++ template-bot.php
@@ -0,0 +1,49 @@
+<?php
+/*
+Template Name: Board of Trustees Template
+*/
+?>
+
+<?php get_template_part('templates/page', 'header'); ?>
+<?php get_template_part('templates/content', 'page'); ?>
+<?php
+ 
+// check if the flexible content field has rows of data
+if( have_rows('board_members') ):
+ 
+     // loop through the rows of data
+    while ( have_rows('board_members') ) : the_row();
+ 
+        if( get_row_layout() == 'single_board_member' ):
+        ?>
+        <div class="row board-member">
+        <div class="col-sm-12"><h2><?php the_sub_field('board_title'); ?></h2></div>
+        <div class="col-sm-3">
+          <?php 
+          $attachment_id = get_sub_field('board_photo');
+          $size = "medium"; // (thumbnail, medium, large, full or custom size)
+           
+          $image = wp_get_attachment_image_src( $attachment_id, $size );
+          ?>
+          <img src="<?php echo $image[0]; ?>" class="img-responsive" />
+        </div>
+        <div class="col-sm-9">
+          <h3 class="board-member-name"><a href="<?php the_sub_field('board_name_link'); ?>"><?php the_sub_field('board_name'); ?></a></h3>
+          <p><a href="<?php the_sub_field('board_podcast_link'); ?>" class="member-podcast"><?php the_sub_field('board_podcast'); ?></a></p>
+          <p><a href="<?php the_sub_field('board_email'); ?>"><i class="fa fa-envelope"></i> <?php the_sub_field('board_email'); ?></a></p>
+        	<?php the_sub_field('position_responsibilities'); ?>
+        
+
+        </div>
+        <?php
+        endif;
+ 
+    endwhile;
+ 
+else :
+ 
+    // no layouts found
+ 
+endif;
+ 
+?>
