45502277cf77b364906d841adc9e4f9786c9597e
Kelly Anne <kpipe@sju.edu>
2014-03-02 17:02:25 -0500

    BOT Styling

--- lib/scripts.php
+++ lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '265ad714228ba62031a205020d3d7a59');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'b8ebbfd51afeb89856d9e67c38effa6c');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
