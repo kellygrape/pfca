51e052d67a978826352e3dd9fbcaa38d75072bd7
Kelly Anne <kpipe@sju.edu>
2014-03-03 20:59:40 -0500

    Styling, homepage posts

--- lib/custom.php
+++ lib/custom.php
@@ -180,3 +180,27 @@ class pfca_hide_user_info_things
         print $html;
     }
 }
+
+
+/**
+ * Redirects admin to the dashboard and others to the homepage
+ *
+ * @return void
+ */
+function my_login_redirect( $redirect_to, $request, $user ){
+    //is there a user to check?
+    global $user;
+    if( isset( $user->roles ) && is_array( $user->roles ) ) {
+        //check for admins
+        if( in_array( "administrator", $user->roles ) ) {
+            // redirect them to the default place
+            return $redirect_to;
+        } else {
+            return home_url();
+        }
+    }
+    else {
+        return $redirect_to;
+    }
+}
+add_filter("login_redirect", "my_login_redirect", 10, 3);
--- lib/scripts.php
+++ lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '7ff55e9ae14796c343f201ea4c0e0633');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '748738a28138b082c6488eb0aadaa1e5');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
