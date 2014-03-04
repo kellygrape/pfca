ad9f66847f73f24ebd26b661e27bdab7f2b855b1
Kelly Anne <kpipe@sju.edu>
2014-03-03 21:30:08 -0500

    Homepage

--- base-front-page.php
+++ base-front-page.php
@@ -21,15 +21,33 @@
     <div class="content row">
       <main class="main <?php echo roots_main_class(); ?>" role="main">
         <?php include roots_template_path(); ?>
+        <h3>Posts</h3>
+        <?php 
+        // the query
+        $the_query = new WP_Query( array('post_type' => 'post') ); ?>
         
-        <?php
-         global $post;
-         $myposts = get_posts('numberposts=10');
-         foreach($myposts as $post) :
-         ?>
-         <?php get_template_part('templates', 'content'); ?>
+        <?php if ( $the_query->have_posts() ) : ?>
         
-         <?php endforeach; ?>
+          <!-- pagination here -->
+        
+          <!-- the loop -->
+          <?php while ( $the_query->have_posts() ) : $the_query->the_post(); ?>
+            <h2><?php the_title(); ?></h2>
+          <?php endwhile; ?>
+          <!-- end of the loop -->
+          <?php if ($wp_query->max_num_pages > 1) : ?>
+            <nav class="post-nav">
+              <ul class="pager">
+                <li class="previous"><?php next_posts_link(__('&larr; Older posts', 'roots')); ?></li>
+                <li class="next"><?php previous_posts_link(__('Newer posts &rarr;', 'roots')); ?></li>
+              </ul>
+            </nav>
+          <?php endif; ?>
+        
+          <?php wp_reset_postdata(); ?>
+        <?php endif; ?>
+        
+        
       </main><!-- /.main -->
       <?php if (roots_display_sidebar()) : ?>
         <aside class="sidebar <?php echo roots_sidebar_class(); ?>" role="complementary">
