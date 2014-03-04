f74b7bc783da9c9cee07cf77b2362c917ab586a2
Kelly Anne <kpipe@sju.edu>
2014-03-03 23:07:46 -0500

    Header

--- lib/scripts.php
+++ lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'dc63ac924fc9ab54ed315aa35db1e793');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '3db85baf08eef20542f08cb4a4ce698f');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
