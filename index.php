a8281cbfeda3219233f7e9c8275c7162d20e0d85
Kelly Anne <kpipe@sju.edu>
2014-03-05 21:47:09 -0500

    Members Archive

--- index.php
+++ index.php
@@ -11,7 +11,7 @@
   <?php if ( 'pfca_podcast' == get_post_type() ): ?>
     <?php get_template_part('templates/excerpt', 'podcast'); ?>
   <?php elseif ( 'pfca_member' == get_post_type() ): ?>
-    <?php get_template_part('templates/content', get_post_format()); ?>
+    <?php get_template_part('templates/content', 'member'); ?>
   <?php else: ?>
     <?php get_template_part('templates/content', get_post_format()); ?>
   <?php endif; ?>
